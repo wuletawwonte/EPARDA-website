ee82220ce68dbfeb17a8878d489d63b76ad9df8d
Wuletaw <wuletaw.wonte@yahoo.com>
2016-12-27 11:36:11 -0800

    after comment

---
+++ public/js/tableFixRows.js
@@ -0,0 +1,17 @@
+
+makeTableScroll();
+
+function makeTableScroll() {
+    var maxRows = 10;
+
+    var table = document.getElementById('donorsTable');
+    var wrapper = table.parentNode;
+    var rowsInTable = table.rows.length;
+    var height = 0;
+    if (rowsInTable > maxRows) {
+        for (var i = 0; i < maxRows; i++) {
+            height += table.rows[i].clientHeight;
+        }
+        wrapper.style.height = height + "px";
+    }
+}
